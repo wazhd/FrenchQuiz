d743e6fb91ad687c134f8b7f167eddcec33c7fca
wazhd <157824420+wazhd@users.noreply.github.com>
2026-03-09 16:06:45 -0400

    Refine quiz questions and user feedback
    Updated quiz questions and feedback messages for clarity.

--- app.py
+++ app.py
@@ -14,27 +14,36 @@ st.write("Testez vos connaissances sur la francophonie et la culture torontoise 
 with st.form("quiz_form"):
     q1 = st.radio(
         "1. En quelle année Samuel de Champlain est-il arrivé pour explorer la région ?",
-        ["1534", "1615", "1790", "1803"]
+        ["1534", "1615", "1790", "1803"],
+        index=None
     )
 
     q2 = st.radio(
         "2. Quel pourcentage de la population de Toronto avait le français comme langue maternelle en 2021 ?",
-        ["1,5 %", "3,7 %", "10 %", "12,4 %"]
+        ["1,5 %", "3,7 %", "10 %", "12,4 %"],
+        index=None
+
     )
 
     q3 = st.radio(
         "3. Quel est l'ingrédient principal qui compose la base de la 'Sushi Pizza' à Toronto ?",
-        ["Pâte à pizza classique", "Pain pita", "Riz frit", "Feuille d'algue séchée"]
+        ["Pâte à pizza classique", "Pain pita", "Riz frit", "Feuille d'algue séchée"],
+        index=None
+
     )
 
     q4 = st.radio(
         "4. Quel quartier est connu pour être le premier projet de logement social au Canada ?",
-        ["Corktown", "Little Italy", "Old Town", "Regent Park"]
+        ["Corktown", "Little Italy", "Old Town", "Regent Park"],
+        index=None
+
     )
 
     q5 = st.radio(
         "5. Pendant combien de temps la Tour CN a-t-elle été la plus haute tour du monde ?",
-        ["10 ans", "20 ans", "Plus de 30 ans", "Elle l'est toujours"]
+        ["10 ans", "20 ans", "Plus de 30 ans", "Elle l'est toujours"],
+        index=None
+
     )
 
     submit_button = st.form_submit_button("Soumettre mes réponses")
@@ -57,6 +66,4 @@ if submit_button:
     elif st.session_state.score >= 3:
         st.info(f"Pas mal ! Ton score est de : {st.session_state.score}/5 👍")
     else:
-        st.warning(f"Tu peux faire mieux ! Score : {st.session_state.score}/5.")
-
-st.sidebar.info("Projet présenté par Ryan, Dravya, Joel et Carson.")
+        st.warning(f"Tu peux faire mieux ! Score : {st.session_state.score}/5. Relis le script !")
